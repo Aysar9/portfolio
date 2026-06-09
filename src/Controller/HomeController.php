cf3b3293db01d598cbfdf54cee36ff6b923988d3
Aysar <Aysar.Alatrash@butting-schwedt.de>
2026-06-09 17:53:45 +0200

    Add about page and link in navigation

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,4 +15,10 @@ final class HomeController extends AbstractController
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/ueber-mich', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
 }
