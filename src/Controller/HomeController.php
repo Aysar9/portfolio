413f6f923fb4f581b2e7c9fdbaf95bf6ce665ee6
Aysar <Aysar.Alatrash@butting-schwedt.de>
2026-06-10 12:40:24 +0200

    Implement internationalization support with German and English translations, update routing for locale handling, and enhance templates for dynamic content rendering.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/{_locale}', requirements: ['_locale' => 'de|en'])]
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
